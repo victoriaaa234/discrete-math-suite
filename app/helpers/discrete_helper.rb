8750bc4fd7ad972e42c631275bc071ccf0c46e08
jason <zuangjason@gmail.com>
2018-01-04 14:19:50 +0700

    rails

---
+++ app/helpers/discrete_helper.rb
@@ -0,0 +1,2 @@
+module DiscreteHelper
+end
---
+++ test/controllers/discrete_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class DiscreteControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
