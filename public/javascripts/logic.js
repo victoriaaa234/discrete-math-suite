e9675b3e903a1719cbf456824d37c0d4f08d27f5
victoriaaa234 <victoriaaa234@tamu.edu>
2018-01-09 16:25:19 +0800

    added expert mode text boxes

--- public/javascripts/logic.js
+++ public/javascripts/logic.js
@@ -1,3 +1,8 @@
+$(document).ready(function(){
+	// the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+	$('#modal1').modal();
+  });
+  
 function addPremiseRow() {
 	var premiseSecondaries = document.getElementById("premise_secondaries");
 	if(premiseSecondaries.children.length > 0) { 
