0bdc3a7aa2b4317726ca20b0e0686c6b6b0ea5c1
victoriaaa234 <victoriaaa234@tamu.edu>
2018-01-15 00:52:53 +0800

    started practice problem option

--- public/javascripts/logic.js
+++ public/javascripts/logic.js
@@ -271,89 +271,98 @@ function deleteProofRow(buttonDivId) {
 }
 */
 
-    function submitText(){
+function generatePracticeProblems() {
+    //TODO(vwei): create database of practice problems and randomly select one.
+    // submitText(premise, conclusion, proof);
+    console.log('Generated practice problem');
+}
+
+function submitText(premise, conclusion, proof) {
+    var premiseList = premise.split(',');
+    var proofList = proof.split('\n');
+
+    //adding the premises
+    for (i = 1; i <= premiseList.length; i++){
+        var premiseId = 'input_premise_'+i;
+        if(document.getElementById(premiseId)!==null){
+            document.getElementById(premiseId).value = premiseList[i-1].trim();
+            console.log(premiseList[i-1]);
+        }
+        else{
+            addPremiseRow();
+            document.getElementById(premiseId).value = premiseList[i-1].trim();
+            console.log(premiseList[i-1]);
+        }
+
+    }
+
+    //removing extra premises
+    var allPremiseBoxesLength = document.querySelectorAll("[id*='input_premise_']").length;
+    if (allPremiseBoxesLength >= i)
+    {
+        for (j = allPremiseBoxesLength; j >= i; j--){
+            var premiseId = 'remove_premise_'+j;
+            deleteRow(premiseId);
+        }
+    }
+
+    //adding the conclusion
+    
+    //remove spaces first
+    conclusion = removeSpaces(conclusion);
+    document.getElementById('input_conclusion_1').value = conclusion;
+    
+    //adding the proofs
+    for (i = 1; i <=proofList.length; i++){
+        var proofs = proofList[i-1].split(' ');
+        var stepId = 'proof_step_input_'+i;
+        var previousId = 'proof_previous_input_'+i;
+        var rulesId = 'proof_rules_input_'+i;
+
+        if(document.getElementById(stepId)!==null){
+            document.getElementById(stepId).value = proofs[0] || "";
+            document.getElementById(previousId).value = proofs[1] || "";
+            var string = " ";
+            for(j = 2;j<proofs.length;j++){
+                string += proofs[j] + " ";
+            }
+            string = string.substr(0, string.length-1);
+            string = removeSpaces(string);
+            document.getElementById(rulesId).value = string;
+        }
+        else{
+            addProofRow();
+            document.getElementById(stepId).value = proofs[0] || "";
+            document.getElementById(previousId).value = proofs[1] || "";
+            document.getElementById(rulesId).value = proofs[2] || "";
+            var string = " ";
+            for(i = 2;i<proofs.length;i++){
+                string += proofs[i] + " ";
+            }
+            document.getElementById(rulesId).value = string;
+        }
+    }
+
+    //removing proofs
+    var allProofRowsLength = document.querySelectorAll("[id*='proof_step_input_']").length;
+    if (i > 1)
+    {
+        if (allProofRowsLength >= i)
+        {
+            for (j = allProofRowsLength; j >= i; j--){
+                var proofRowId = 'proof_remove_'+j;
+                deleteRow(proofRowId);
+            }
+        }
+    }
+}
+    function submitExpertMode(){
         console.log('Submitted');
         var premise = document.getElementById('premise').value;
         var conclusion = document.getElementById('conclusion').value;
         var proof = document.getElementById('proof').value;
 
-        var premiseList = premise.split(',');
-        var proofList = proof.split('\n');
-
-        //adding the premises
-        for (i = 1; i <= premiseList.length; i++){
-            var premiseId = 'input_premise_'+i;
-            if(document.getElementById(premiseId)!==null){
-                document.getElementById(premiseId).value = premiseList[i-1].trim();
-                console.log(premiseList[i-1]);
-            }
-            else{
-                addPremiseRow();
-                document.getElementById(premiseId).value = premiseList[i-1].trim();
-                console.log(premiseList[i-1]);
-            }
-
-        }
-
-        //removing extra premises
-        var allPremiseBoxesLength = document.querySelectorAll("[id*='input_premise_']").length;
-        if (allPremiseBoxesLength >= i)
-        {
-            for (j = allPremiseBoxesLength; j >= i; j--){
-                var premiseId = 'remove_premise_'+j;
-                deleteRow(premiseId);
-            }
-        }
-
-        //adding the conclusion
-        
-        //remove spaces first
-        conclusion = removeSpaces(conclusion);
-        document.getElementById('input_conclusion_1').value = conclusion;
-        
-        //adding the proofs
-        for (i = 1; i <=proofList.length; i++){
-            var proofs = proofList[i-1].split(' ');
-            var stepId = 'proof_step_input_'+i;
-            var previousId = 'proof_previous_input_'+i;
-            var rulesId = 'proof_rules_input_'+i;
-
-            if(document.getElementById(stepId)!==null){
-                document.getElementById(stepId).value = proofs[0] || "";
-                document.getElementById(previousId).value = proofs[1] || "";
-                var string = " ";
-                for(j = 2;j<proofs.length;j++){
-                    string += proofs[j] + " ";
-                }
-                string = string.substr(0, string.length-1);
-                string = removeSpaces(string);
-                document.getElementById(rulesId).value = string;
-            }
-            else{
-                addProofRow();
-                document.getElementById(stepId).value = proofs[0] || "";
-                document.getElementById(previousId).value = proofs[1] || "";
-                document.getElementById(rulesId).value = proofs[2] || "";
-                var string = " ";
-                for(i = 2;i<proofs.length;i++){
-                    string += proofs[i] + " ";
-                }
-                document.getElementById(rulesId).value = string;
-            }
-        }
-
-        //removing proofs
-        var allProofRowsLength = document.querySelectorAll("[id*='proof_step_input_']").length;
-        if (i > 1)
-        {
-            if (allProofRowsLength >= i)
-            {
-                for (j = allProofRowsLength; j >= i; j--){
-                    var proofRowId = 'proof_remove_'+j;
-                    deleteRow(proofRowId);
-                }
-            }
-        }
+        submitText(premise, conclusion, proof);
     }
 
 function removeSpaces(text) {  
