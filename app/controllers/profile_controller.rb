5ee98f282b72019c16ffe33deccc7ac21e485dac
Tyler J. Durden <durden@tamu.edu>
2018-01-22 14:10:26 -0600

    Using erb to properly generate topic cards

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,6 +1,6 @@
 class ProfileController < ApplicationController
 	def profilePage
-
+		@topics = ["Identity Laws", "Domination Laws", "Idempotent Laws", "Double Negation Laws", "Commutative Laws", "Associative Laws", "Distributive Laws", "DeMorgan's Laws", "Absorption Laws", "Negation Laws", "Logical Equivalence Conditional", "Logical Equivalence Biconditional", "De Morgan Laws", "Modus Ponens", "Modus Tollens", "Hypothetical Syllogism", "Disjunctive Syllogism", "Addition", "Simplification", "Conjunction", "Resolution", "Universal Instantiation", "Universal Generalization", "Existential Instantiation", "Existential Generalization", "Random Completions"]
 	end
 end
 
