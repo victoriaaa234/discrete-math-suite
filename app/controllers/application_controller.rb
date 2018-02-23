c661e3725b99a59b6ecfd832ebe8e23565c6faab
amyjiayao <amyjiayao@gmail.com>
2018-02-23 15:58:51 -0600

    update display

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  rescue ActiveRecord::RecordNotFound
   end
 
   before_action :set_last_seen_at, if: proc { !current_user.nil? }
