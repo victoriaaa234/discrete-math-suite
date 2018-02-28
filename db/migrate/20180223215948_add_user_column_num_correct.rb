975f42d607fa975569fddd341083ef639233d0af
MHeinonen <Marcusheinonen1@gmail.com>
2018-02-28 10:03:58 -0600

    migration cast fix

--- db/migrate/20180223215948_add_user_column_num_correct.rb
+++
@@ -1,5 +0,0 @@
-class AddUserColumnNumCorrect < ActiveRecord::Migration[5.1]
-  def change
-    add_column :users, :num_correct, :string
-  end
-end
--- db/migrate/20180223232512_change_string_to_int.rb
+++ db/migrate/20180223232512_change_string_to_int.rb
@@ -1,6 +1,6 @@
 class ChangeStringToInt < ActiveRecord::Migration[5.1]
   def change
-    change_column :users, :num_correct, :integer
-    change_column :users, :num_attempt, :integer
+    add_column :users, :num_correct, :integer
+    add_column :users, :num_attempt, :integer
   end
 end
