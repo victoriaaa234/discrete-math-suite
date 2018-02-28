d6549d633c18495601134dba8ff7276bed2f253f
MHeinonen <Marcusheinonen1@gmail.com>
2018-02-28 10:10:08 -0600

    migrations

--- db/migrate/20180223232512_change_string_to_int.rb
+++ db/migrate/20180223232512_change_string_to_int.rb
@@ -1,6 +1,6 @@
 class ChangeStringToInt < ActiveRecord::Migration[5.1]
   def change
-    change_column :users, :num_correct, :integer
-    change_column :users, :num_attempt, :integer
+    change_column :users, :num_correct, 'integer USING CAST(num_correct AS integer)'
+    change_column :users, :num_attempt, 'integer USING CAST(num_attempt AS integer)'
   end
 end
